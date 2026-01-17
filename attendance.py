057da7b78a7cbb31cdea201a63075aab7d661833
bhagyashreemath68math <bhagyashreemath68@gmail.com>
2026-01-17 14:07:56 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -8,15 +8,15 @@ else:
     script_name = sys.argv[0]
     classes_held = input("Enter number of classes held: ")
     classes_attended = input("Enter number of classes attended: ")
-    percentage = (int(classes_attended) / int(classes_held)) * 100
-
+    classes_held = int(classes_held)
+    classes_attended = int(classes_attended)
+    if classes_held <= 0 or classes_attended < 0 or classes_attended > classes_held:
+        raise ValueError
+    percentage = (classes_attended / classes_held) * 100
     if percentage >= 75:
-        print("Eligible")
         status = "Eligible"
     else:
-        print("Not Eligible")
         status = "Not Eligible"
-    
     print("Script Name:", script_name)
     print("Classes Held:", classes_held)
     print("Classes Attended:", classes_attended)
