c3d6823d45701f949a9266354cfe5ed615e9f699
bhagyashreemath68math <bhagyashreemath68@gmail.com>
2026-01-17 14:19:08 +0530

    Refactor attendance script to use command-line args

--- attendance.py
+++ attendance.py
@@ -1,24 +1,22 @@
 import sys
+
+# Check for command-line arguments
 if len(sys.argv) == 3:
     script_name = sys.argv[0]
-    classes_held = sys.argv[1]
-    classes_attended = sys.argv[2]
-
+    classes_held = int(sys.argv[1])
+    classes_attended = int(sys.argv[2])
 else:
-    script_name = sys.argv[0]
-    classes_held = input("Enter number of classes held: ")
-    classes_attended = input("Enter number of classes attended: ")
-    classes_held = int(classes_held)
-    classes_attended = int(classes_attended)
-    if classes_held <= 0 or classes_attended < 0 or classes_attended > classes_held:
-        raise ValueError
-    percentage = (classes_attended / classes_held) * 100
-    if percentage >= 75:
-        status = "Eligible"
-    else:
-        status = "Not Eligible"
-    print("Script Name:", script_name)
-    print("Classes Held:", classes_held)
-    print("Classes Attended:", classes_attended)
-    print("Attendance Percentage:", percentage)
-    print("Print Status:", status)
+    print("Usage: python attendance.py <classes_held> <classes_attended>")
+    sys.exit(1)
+
+percentage = (classes_attended / classes_held) * 100
+print("Script Name          :", script_name)
+print("Classes Held         :", classes_held)
+print("Classes Attended     :", classes_attended)
+print("Attendance Percentage:", percentage)
+
+if percentage >= 75:
+    print("Status               : Eligible")
+else:
+    print("Status               : Not Eligible")
+    print("Note                 : Minimum 75% attendance required")
